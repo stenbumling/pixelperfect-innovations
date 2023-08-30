fb473575c842fdc6fde0926b90292f09ebd6658d
stenbumling <marcusrosin@outlook.com>
2023-08-30 17:01:35 +0200

    config: added env variable to maps api key

--- src/assets/Home/Map/MapSection.tsx
+++ src/assets/Home/Map/MapSection.tsx
@@ -1,3 +1,5 @@
+const { VITE_GOOGLE_MAPS_API_KEY } = import.meta.env;
+
 export default function MapSection() {
   return (
     <>
@@ -6,7 +8,7 @@ export default function MapSection() {
         height="450"
         style={{ border: "0" }}
         loading="lazy"
-        src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJPe7L6Q3zT0YR9m2RYyuQP7s&key=AIzaSyCUmpOKvZnfyF4aRfNppk9qKCQOi7BlTK0"
+        src={`https://www.google.com/maps/embed/v1/place?q=place_id:ChIJPe7L6Q3zT0YR9m2RYyuQP7s&key=${VITE_GOOGLE_MAPS_API_KEY}`}
       />
     </>
   );
