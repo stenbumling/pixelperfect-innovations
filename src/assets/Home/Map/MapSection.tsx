af8332206e78c8ccc112ad652ca912716cf2e676
stenbumling <marcusrosin@outlook.com>
2023-08-30 16:13:56 +0200

    feat: add simple embed for Google Maps

--- src/assets/Home/Map/MapSection.tsx
+++ src/assets/Home/Map/MapSection.tsx
@@ -1,3 +1,13 @@
 export default function MapSection() {
-  return <></>;
+  return (
+    <>
+      <iframe
+        width="1000"
+        height="450"
+        style={{ border: "0" }}
+        loading="lazy"
+        src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJPe7L6Q3zT0YR9m2RYyuQP7s&key=AIzaSyCUmpOKvZnfyF4aRfNppk9qKCQOi7BlTK0"
+      />
+    </>
+  );
 }
