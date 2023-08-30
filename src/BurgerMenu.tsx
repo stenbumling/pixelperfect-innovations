59ba9f1a731ea8fa91d84d0e73a524cae4e74cf7
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2023-08-30 15:41:10 +0200

    make animation for burger on click

--- src/BurgerMenu.tsx
+++ src/BurgerMenu.tsx
@@ -1,12 +1,16 @@
+import { useState } from "react";
 import styled from "styled-components";
 
 export default function BurgerMenu() {
-  const toggleMenu = () => {};
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
 
   return (
     <>
       <StyledBurgerMenu onClick={toggleMenu}>
-        <Burger />
+        <Burger $menuOpen={menuOpen} />
       </StyledBurgerMenu>
     </>
   );
@@ -20,31 +24,36 @@ const StyledBurgerMenu = styled.div`
   mix-blend-mode: difference;
 `;
 
-const Burger = styled.div`
+const Burger = styled.div<{ $menuOpen: boolean }>`
   width: 1.9rem;
   height: 0.2rem;
   position: relative;
-  top: 50%;
-  background-color: #fff;
+  top: 0;
+  background-color: ${({ $menuOpen }) =>
+    $menuOpen === true ? "transparent" : "#fff"};
 
   &::after,
   &::before {
     content: "";
     position: absolute;
     background-color: inherit;
+    background-color: #fff;
+    transition: width 0.4s ease;
+    transition: rotate 0.5s 0.2s ease;
     width: inherit;
     height: inherit;
-    top: 0;
     transition: 0.3s ease;
   }
 
   &::after {
-    top: -0.6rem;
+    rotate: ${({ $menuOpen }) => ($menuOpen ? "45deg" : "0")};
+    top: ${({ $menuOpen }) => ($menuOpen ? "0" : "-.6rem")};
   }
 
   &::before {
-    width: 1rem;
-    top: 0.6rem;
+    width: ${({ $menuOpen }) => ($menuOpen ? "1.9rem " : "1rem")};
+    rotate: ${({ $menuOpen }) => ($menuOpen ? "-45deg" : "0")};
+    top: ${({ $menuOpen }) => ($menuOpen ? "0" : ".6rem")};
     right: 0;
   }
 `;
