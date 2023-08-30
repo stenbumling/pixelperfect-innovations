ee7117f29f9eaabac91d08822dd2bbe0d9e2844a
Gabriel Lugo Méndez <gabriel.lugo.mendez@gmail.com>
2023-08-30 13:35:14 +0200

    add icons

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,49 @@
+import {
+  IconBrandInstagram,
+  IconBrandLinkedin,
+  IconBrandPinterest,
+  IconBrandTwitterFilled,
+} from "@tabler/icons-react";
+import styled from "styled-components";
+
+const FooterContainer = styled.footer`
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem;
+`;
+
+const ContentDiv = styled.div`
+  flex: 1;
+  text-align: left;
+`;
+
 function Footer() {
   return (
-    <footer>
-      <h1>Footer</h1>
-    </footer>
+    <FooterContainer>
+      <ContentDiv>
+        <h3>Contact</h3>
+        <p>PixelPerfect Innovations</p>
+        <p>Anders Personsgatan 18,</p>
+        <p>416 64 Göteborg</p>
+        <p>info@pixelperfect.com</p>
+        <p>031-83 28 31</p>
+      </ContentDiv>
+      <ContentDiv>
+        <h3>Work with us</h3>
+        <p>We are always looking for new people to join our team!</p>
+        <p>work@pixelperfect.com</p>
+      </ContentDiv>
+      <ContentDiv>
+        <h3>Follow</h3>
+        {/* Add your social media icons here */}
+        <div>
+          <IconBrandLinkedin />
+          <IconBrandInstagram />
+          <IconBrandPinterest />
+          <IconBrandTwitterFilled />
+        </div>
+      </ContentDiv>
+    </FooterContainer>
   );
 }
 
