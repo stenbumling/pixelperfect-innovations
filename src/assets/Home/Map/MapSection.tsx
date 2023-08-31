3623a1b27515aab218abfa5a6596261af47ea85d
stenbumling <marcusrosin@outlook.com>
2023-08-31 20:54:45 +0200

    feat: add content for the map section around the interactive map

--- src/assets/Home/Map/MapSection.tsx
+++ src/assets/Home/Map/MapSection.tsx
@@ -1,5 +1,41 @@
-const { VITE_GOOGLE_MAPS_API_KEY } = import.meta.env;
+import styled from "styled-components";
+import InteractiveMap from "./InteractiveMap";
 
 export default function MapSection() {
-  return (<></>)
+  return (
+    <>
+      <div className="grid-container">
+        <Title>Find us</Title>
+      </div>
+      <div className="grid-container">
+        <div className="grid">
+          <InteractiveMap />
+        </div>
+        <AddressContainer>
+          <CompanyTitle className="grid">PixelPerfect Innovations</CompanyTitle>
+          <div className="grid">Anders Personsgatan 18,</div>
+          <div className="grid">416 64 Göteborg</div>
+        </AddressContainer>
+      </div>
+    </>
+  );
 }
+
+const Title = styled.h3`
+  grid-column: main;
+  font-size: var(--font-size-m);
+  padding-bottom: 1rem;
+`;
+
+const AddressContainer = styled.div`
+  grid-column: main;
+  display: flex;
+  flex-direction: column;
+  padding-top: 2rem;
+  padding-bottom: 8rem;
+`;
+
+const CompanyTitle = styled.h4`
+  grid-column: main;
+  font-size: var(--font-size-s);
+`;
