2a54d3837ae1d54d8d8afba1b31e503efa8df520
stenbumling <marcusrosin@outlook.com>
2023-08-31 20:24:30 +0200

    dep: add react-google-maps dependency for api

--- src/assets/Home/Map/MapSection.tsx
+++ src/assets/Home/Map/MapSection.tsx
@@ -1,15 +1,5 @@
 const { VITE_GOOGLE_MAPS_API_KEY } = import.meta.env;
 
 export default function MapSection() {
-  return (
-    <>
-      <iframe
-        width="1000"
-        height="450"
-        style={{ border: "0" }}
-        loading="lazy"
-        src={`https://www.google.com/maps/embed/v1/place?q=place_id:ChIJPe7L6Q3zT0YR9m2RYyuQP7s&key=${VITE_GOOGLE_MAPS_API_KEY}`}
-      />
-    </>
-  );
+  return (<></>)
 }
