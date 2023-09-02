364259611a5c1159f0bf59e71000d97a25483173
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-09-02 23:22:11 +0200

    Added about section to profile page

--- src/assets/Profile/ProfilePage.tsx
+++ src/assets/Profile/ProfilePage.tsx
@@ -1,10 +1,12 @@
 import Footer from '../../Footer';
 import Header from '../../Header';
+import AboutSection from './About/AboutSection';
 
 function ProfilePage() {
   return (
     <>
       <Header />
+      <AboutSection />
       <Footer />
     </>
   );
