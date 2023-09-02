38c2f58f721746e70b94d4a26e11e28c66229240
lisamarieandersson <lisamarie.andersson@gmail.com>
2023-09-02 23:22:48 +0200

    added name and image

--- src/assets/Profile/About/AboutSection.tsx
+++ src/assets/Profile/About/AboutSection.tsx
@@ -1,3 +1,19 @@
+import { useParams } from 'react-router-dom';
+import teamMembers from '../../../../data/team';
+
 export default function AboutSection() {
-  return <></>;
+  const { id } = useParams();
+
+  const teamMember = teamMembers.find((member) => member.id === id);
+
+  if (!teamMember) {
+    return <div>Team member not found!</div>;
+  }
+
+  return (
+    <div>
+      <h2>About {teamMember.name}</h2>
+      <img src={teamMember.image} alt={teamMember.name} />
+    </div>
+  );
 }
