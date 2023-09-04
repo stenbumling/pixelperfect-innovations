61d60e7b14e123758e27b831c79e6c8770a16dbf
Gabriel Lugo Méndez <gabriel.lugo.mendez@gmail.com>
2023-09-04 16:26:06 +0200

    add padding correct placement of section

--- src/assets/Home/Map/MapSection.tsx
+++ src/assets/Home/Map/MapSection.tsx
@@ -10,23 +10,31 @@ export default function MapSection({ id }: SectionProps) {
 
   return (
     <div id={id}>
-      <div className="grid-container">
-        <Title>Find us</Title>
-      </div>
-
-      <div className="grid-container">
-        <div className="grid">
-          {isLoaded ? <InteractiveMap /> : <div>Loading map...</div>}
+      <MapContainer>
+        <div className="grid-container">
+          <Title>Find us</Title>
         </div>
-        <AddressContainer>
-          <CompanyTitle className="grid">PixelPerfect Innovations</CompanyTitle>
-          <div className="grid">Anders Personsgatan 18</div>
-          <div className="grid">416 64 Göteborg</div>
-        </AddressContainer>
-      </div>
+
+        <div className="grid-container">
+          <div className="grid">
+            {isLoaded ? <InteractiveMap /> : <div>Loading map...</div>}
+          </div>
+          <AddressContainer>
+            <CompanyTitle className="grid">
+              PixelPerfect Innovations
+            </CompanyTitle>
+            <div className="grid">Anders Personsgatan 18</div>
+            <div className="grid">416 64 Göteborg</div>
+          </AddressContainer>
+        </div>
+      </MapContainer>
     </div>
   );
 }
+
+const MapContainer = styled.div`
+  padding-top: 5rem;
+`;
 
 const Title = styled.h3`
   grid-column: main;
