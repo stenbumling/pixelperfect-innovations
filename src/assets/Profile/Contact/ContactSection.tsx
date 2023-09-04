03d73299dd3aa08a3ee5513ae0f7e1195f36cf35
stenbumling <marcusrosin@outlook.com>
2023-09-04 08:20:09 +0200

    feat: add basic form

--- src/assets/Profile/Contact/ContactSection.tsx
+++ src/assets/Profile/Contact/ContactSection.tsx
@@ -1,14 +1,39 @@
 import styled from "styled-components";
 
-export default function MapSection() {
+export default function ContactSection() {
   return (
     <>
       <div className="grid-container">
-        <Title>Contact</Title>
+        <Title>Contact me</Title>
       </div>
 
       <div className="grid-container">
-        <div className="grid">A beautiful contact form is on it's way</div>
+        <div className="grid">
+          <ContactForm>
+            <ContactFormRow>
+              <label>
+                First Name
+                <input type="text" name="firstName" />
+              </label>
+              <label>
+                Last Name
+                <input type="text" name="lastName" />
+              </label>
+            </ContactFormRow>
+            <ContactFormRow>
+              <label>
+                Email Address
+                <input type="email" name="email" />
+              </label>
+            </ContactFormRow>
+            <ContactFormRow>
+              <label>
+                Message
+                <textarea name="message" />
+              </label>
+            </ContactFormRow>
+          </ContactForm>
+        </div>
       </div>
     </>
   );
@@ -18,4 +43,20 @@ const Title = styled.h3`
   grid-column: main;
   font-size: var(--font-size-m);
   padding-bottom: 2rem;
+`;
+
+const ContactForm = styled.form`
+  max-width: 600px;
+`;
+
+const ContactFormRow = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 16px;
+
+  &:first-child {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+  }
 `;
