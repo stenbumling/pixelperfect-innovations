af96e2171d03180c9baeee2e9da0297fc89ec108
Ellen Dahlgren <ellensofiadahlgren@outlook.com>
2023-09-06 10:41:09 +0200

    add a title in Case containing the brand + changed data

--- data/case.ts
+++ data/case.ts
@@ -8,37 +8,37 @@ export interface Case {
 
 const cases: Case[] = [
   {
-    id: '01',
-    image: '/videos/image1.png',
-    video: '/videos/video1.mp4',
+    id: "01",
+    image: "/videos/image1.png",
+    video: "/videos/video1.mp4",
     description:
-      "We brought TechVanguard's innovative 3D modeling vision to life, combining creativity with cutting-edge technology for stunning visual impact.",
+      "We brought a innovative 3D modeling vision to life, combining creativity with cutting-edge technology for stunning visual impact.",
 
-    brand: 'TechVanguard',
+    brand: "TechVanguard",
   },
   {
-    id: '02',
-    image: '/videos/image2.png',
-    video: '/videos/video2.mp4',
+    id: "02",
+    image: "/videos/image2.png",
+    video: "/videos/video2.mp4",
     description:
-      'Geomagic Wonders pushed the boundaries of design with an experimental 3D geometric model',
-    brand: 'Geomagic Wonders',
+      "Pushed the boundaries of design with an experimental 3D geometric model",
+    brand: "Geomagic Wonders",
   },
   {
-    id: '03',
-    image: '/videos/image3.png',
-    video: '/videos/video3.mp4',
+    id: "03",
+    image: "/videos/image3.png",
+    video: "/videos/video3.mp4",
     description:
-      'We developed a user-friendly webshop for Rent a Studio, streamlining the rental process for photo studios and equipment.',
-    brand: 'Rent a Studio',
+      "We developed a user-friendly webshop streamlining the rental process for photo studios and equipment.",
+    brand: "Rent a Studio",
   },
   {
-    id: '04',
-    image: '/videos/image4.png',
-    video: '/videos/video4.mp4',
+    id: "04",
+    image: "/videos/image4.png",
+    video: "/videos/video4.mp4",
     description:
-      'Our team created an advertisement film for UrbanRide and developed their website, resulting in increased brand awareness and customer engagement.',
-    brand: 'UrbanRide',
+      "Our team created a commercial and website, enhancing brand awareness and customer engagement.",
+    brand: "UrbanRide",
   },
 ];
 
