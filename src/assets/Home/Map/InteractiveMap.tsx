e635f198ffc1c2b97ce657d44b2ab0a2a6506460
stenbumling <marcusrosin@outlook.com>
2023-09-06 14:10:25 +0200

    feat: add infowindow to marker on map

--- src/assets/Home/Map/InteractiveMap.tsx
+++ src/assets/Home/Map/InteractiveMap.tsx
@@ -1,8 +1,9 @@
-import { GoogleMap, MarkerF } from '@react-google-maps/api';
-import { useMemo } from 'react';
+import { GoogleMap, InfoWindowF, MarkerF } from '@react-google-maps/api';
+import { useMemo, useState } from 'react';
 import styled from 'styled-components';
 
 export default function InteractiveMap() {
+  const [isOpen, setIsOpen] = useState(false);
   const address = useMemo(
     () => ({ lat: 57.70992004609615, lng: 11.994712202229747 }),
     []
@@ -17,6 +18,10 @@ export default function InteractiveMap() {
     []
   );
 
+  const toggleOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <Container>
       <GoogleMap
@@ -27,11 +32,21 @@ export default function InteractiveMap() {
       >
         <MarkerF
           position={address}
+          onClick={toggleOpen}
           icon={{
             url: '/icons/map-marker-icon.svg',
             scaledSize: new window.google.maps.Size(40, 40),
           }}
         />
+        {isOpen && (
+          <InfoWindowF position={address} onCloseClick={toggleOpen}>
+            <div>
+              <h5>PixelPerfect Innovations</h5>
+              <p>Anders Personsgatan 18</p>
+              <p>416 64 Göteborg</p>
+            </div>
+          </InfoWindowF>
+        )}
       </GoogleMap>
     </Container>
   );
